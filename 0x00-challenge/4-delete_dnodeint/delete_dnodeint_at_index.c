1205645586d3d5bd68242f4e90a89ea26307ee9a
NyatiZw <appsmaid19@gmail.com>
2023-05-28 20:47:49 -0700

    clanguage

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -12,34 +12,42 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *saved_head;
+	dlistint_t *tmp;
 	unsigned int p;
 
 	if (*head == NULL)
 	{
 		return (-1);
 	}
-	
 	saved_head = *head;
-	if (index == 0)
+	p = 0;
+	while (p < index && *head != NULL)
 	{
-		*head = saved_head->next;
-		if (*head != NULL)
-			(*head)->prev = NULL:
-		free(saved_head);
-		return 1;
+		*head = (*head)->next;
+		p++;
 	}
-
-	for (p = 0; p < index; p++)
+	if (p != index)
 	{
-		if (saved_head == NULL)
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
 		{
-			return -1;
-			saved_head = saved_head->next;
+			tmp->prev = NULL;
 		}
-	saved_head->pre->next = saved_head->next;
-	if (saved_head->next != NULL)
-		saved_head->next->prev = saved_head;
-	free(saved_head);
-
-	return 1;
+	}
+	else
+	{
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
